acc55e2376a7111c983fa30471d9b405cf6b1fbf
Cassandra Rommel <Cassie@wireless-165-124-137-215.nuwlan.northwestern.edu>
2013-05-08 18:16:19 -0500

    Got lax about committing! This is the graph api and sorting by terms and lots of stuff. oops!

--- front-end/javascript.js
+++ front-end/javascript.js
@@ -1,9 +1,57 @@
-function httpGet(theUrl)
+var obama = new Array();
+var romney = new Array();
+var obama_data = new Array();
+var romney_data = new Array();
+var obama_dates = ['06.12.12', '06.22.12', '07.10.12','07.19.12', '07.23.12', '07.27.12', '08.01.12', '08.09.12', '08.12.12', '08.14.12',
+	'09.07.12', '09.09.12', "09.17.12","09.18.12","09.21.12",'10.04.12', "10.19.12","10.25.12", '11.05.12', "11.06.12"];
+var romney_dates = ['06.15.12', '06.21.12', '07.11.12', '07.24.12', '07.29.12', '07.31.12', '08.10.12', '08.15.12', 
+	'08.29.12', '08.30.12', '09.11.12', '09.17.12', '09.25.12', '10.08.12', '10.19.12', '10.26.12', '11.02.12', '11.06.12'];
+//var obama_dates = ['06.12.12'];
+//var romney_dates = ['06.15.12'];
+
+var terms = new Array();
+var j; //keeping track of when we've been through all the dates
+
+//tabs
+var tabLinks = new Array();
+var contentDivs = new Array();
+
+function collectData(topic) {
+	//Obama
+	if(j<obama_dates.length) {
+		ob_data = eval(httpGet('http://localhost:8080/pres_rhetoric_scrape_obama', obama_dates[j], topic));
+		if(ob_data.length>0) {
+			var date = createJSDate(ob_data[0].date);
+			var count = termCountTotal(ob_data[0].terms);
+			obama.push([date, count]);
+			addTerms(ob_data[0].terms, terms);
+			
+			obama_data.push(ob_data[0]);
+		}
+	}
+	//Romney
+	if(j<romney_dates.length) {
+		ro_data = eval(httpGet('http://localhost:8080/pres_rhetoric_scrape_romney', romney_dates[j], topic));
+		if(ro_data.length>0) {
+			var date = createJSDate(ro_data[0].date);
+			var count = termCountTotal(ro_data[0].terms);
+			romney.push([date, count]);
+			addTerms(ro_data[0].terms, terms);
+			
+			romney_data.push(ro_data[0]);
+		}
+	}
+
+	j=j+1;
+};
+
+function httpGet(theUrl, date, topic)
 {
 	var xmlHttp = null;
 
 	xmlHttp = new XMLHttpRequest();
-	xmlHttp.open( "GET", theUrl, false );
+	params = "date=" + date + "&topic=" + topic
+	xmlHttp.open( "GET", theUrl + "?" + params, false );
 	try {
 		xmlHttp.send(null);
 	}
@@ -11,5 +59,194 @@ function httpGet(theUrl)
 		console.log(e);
 		console.log(xmlHttp);
 	}
-	document.getElementById("graph").innerHTML=xmlHttp.responseText;
+	return xmlHttp.responseText;
 }
+
+function graph(topic) 
+{
+		j=0;
+		obama = [];
+		romney = [];
+		obama_data = [];
+		romney_data = [];
+		terms = [];
+		var updateInterval = 30;
+
+		function showTooltip(x, y, contents) {
+			$("<div id='tooltip'>" + contents + "</div>").css({
+				position: "absolute",
+				display: "none",
+				top: y + 5,
+				left: x + 5,
+				border: "1px solid #fdd",
+				padding: "2px",
+				"background-color": "#fee",
+				opacity: 0.80,
+				"list-style-type" : "none",
+			}).appendTo("body").fadeIn(200);
+		}
+
+		function update(topic) {
+			collectData(topic);
+			$.plot("#placeholder", [
+				{ color: "red", data: obama, label: "Obama"},
+				{ color: "blue", data: romney, label: "Romney"}
+			], 
+			{
+				series: {
+					lines: {
+						show: true
+					},
+					points: {
+						show: true
+					}
+				},
+				grid: {
+					hoverable: true,
+					clickable: true
+				},
+				xaxis: { 
+					mode: "time",
+					min: (new Date(2012, 05, 12)).getTime(),
+					max: (new Date(2012, 10, 06)).getTime()
+				}
+			});
+
+			$("#placeholder").bind("plothover", function (event, pos, item) {
+				if (item) {
+					$("#tooltip").remove();
+					//var x = item.datapoint[0].toFixed(2),
+					//y = item.datapoint[1].toFixed(2);
+
+					var data = {};
+					if(item.series.label == "Romney") {
+						data = romney_data[item.dataIndex].terms;
+					}
+					else if (item.series.label == "Obama") {
+						data = obama_data[item.dataIndex].terms;
+					}
+					term_array = "";
+					for(var i=0; i<data.length; i++) {
+						term_array = term_array + data[i][0] + " : " + data[i][1] + "<br />";
+					}
+					showTooltip(item.pageX, item.pageY,
+				    "Term Counts: <br />" + term_array);
+				}
+			});
+
+			$("#placeholder").bind("plotclick", function (event, pos, item) {
+				data = "";
+				if(item.series.label == "Romney") {
+						data = romney_data[item.dataIndex].full_url;
+				}
+				else if (item.series.label == "Obama") {
+						data = obama_data[item.dataIndex].full_url;
+				}
+			
+				window.location = data;
+			});
+
+			if(j<obama_dates.length || j<romney_dates.length) {
+				setTimeout(function() {update(topic);}, updateInterval);
+			}
+		}
+
+		update(topic);
+
+		// Add the Flot version string to the footer
+
+		$("#footer").prepend("Flot " + $.plot.version + " &ndash; ");
+};
+
+	function termCountTotal(termArray) {
+		total = 0;
+		for(var i=0;i<termArray.length;i++) {
+			total+=termArray[i][1];
+		}
+		return total;
+	}
+
+	function createJSDate(dateString) {
+		var date = dateString.split(".");
+		//Adjust the date
+		date[2] = "20" + date[2];
+		date[0] = date[0] - 1;
+		return new Date(date[2], date[0], date[1])
+	}
+
+	function addTerms(termArray, terms) {
+		for(var i=0;i<termArray.length;i++) {
+			if(($.inArray(termArray[i][0], terms))==-1) {
+				terms.push(termArray[i][0]);
+			}
+		}
+
+		document.getElementById("terms").innerHTML = "Terms we looked for included: " + terms;
+	}
+
+//Tabbing features
+
+    function init() {
+
+      // Grab the tab links and content divs from the page
+      var tabListItems = document.getElementById('tabs').childNodes;
+      for ( var i = 0; i < tabListItems.length; i++ ) {
+        if ( tabListItems[i].nodeName == "LI" ) {
+          var tabLink = getFirstChildWithTagName( tabListItems[i], 'A' );
+          var id = getHash( tabLink.getAttribute('href') );
+          tabLinks[id] = tabLink;
+          contentDivs[id] = document.getElementById( id );
+        }
+      }
+
+      // Assign onclick events to the tab links, and
+      // highlight the first tab
+      var i = 0;
+
+      for ( var id in tabLinks ) {
+        tabLinks[id].onclick = showTab;
+        tabLinks[id].onfocus = function() { this.blur() };
+        if ( i == 0 ) tabLinks[id].className = 'selected';
+        i++;
+      }
+
+      // Hide all content divs except the first
+      var i = 0;
+
+      for ( var id in contentDivs ) {
+        if ( i != 0 ) contentDivs[id].className = 'tabContent hide';
+        i++;
+      }
+
+    }
+
+    function showTab() {
+      var selectedId = getHash( this.getAttribute('href') );
+
+      // Highlight the selected tab, and dim all others.
+      // Also show the selected content div, and hide all others.
+      for ( var id in contentDivs ) {
+        if ( id == selectedId ) {
+          tabLinks[id].className = 'selected';
+          contentDivs[id].className = 'tabContent';
+          graph(selectedId);
+        } else {
+          tabLinks[id].className = '';
+          contentDivs[id].className = 'tabContent hide';
+        }
+      }
+
+      // Stop the browser following the link
+      return false;
+    }
+
+    function getFirstChildWithTagName( element, tagName ) {
+      for ( var i = 0; i < element.childNodes.length; i++ ) {
+        if ( element.childNodes[i].nodeName == tagName ) return element.childNodes[i];
+      }
+    }
+
+    function getHash( url ) {
+      var hashPos = url.lastIndexOf ( '#' );
+      return url.substring( hashPos + 1 );
+    }
--- pyramid/simple.py
+++ pyramid/simple.py
@@ -3,24 +3,96 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 from bs4 import BeautifulSoup
 import urllib2
+import datetime
+import re
+from collections import Counter
+import json
+
+#GLOBAL VARIABLES
+start_date = datetime.datetime(2012,06,07)
+last_date = datetime.datetime(2012,11,07)
+difference = (last_date - start_date).days
+black_list = ["the", "to", "and", "of", "that", "a", "we", "you", "in", "i", "for", "will", "our", "this", "they", "is", "it", "not", "have", "because",
+"going", "are", "on", "td", "tr", "what", "can", "has", "/p", "us", "applause", "just", "if", "got", "more", "make", "need", "here", "their", "who", "be",
+"by", "with", "/tr", "do", "now", "up", "your", "or", "when", "from", "an", "very", "var", "we've", "we're", "that's", "was", "about", "that's", "my", 
+"so", "but", "don't", "get", "he", "were", "all", "back", "p", "all", "new"]
+#END GLOBALS
+
+immigration_list = ["immigration", "immigrant", "immigrants", "dream", "deport", "deported", "deportation", "illegal", "citizen","citizenship"]
+foreignAffairs_list = ["war", "veteran", "veterans", "military", "navy", "army", "foreign", "syria", "syrian", "syrians", "afghanistan", "afghan", "karzai", "troops", "iraq", "iran", "nuclear", "pentagon", "laden", "guantanamo", "abroad", "benghazi", "qaeda"]
+education_list = ["education", "teacher", "teachers", "school", "schools", "student", "students", "classroom", "classrooms", "college", "colleges", "university", "universities", "tuition"]
 
 def hello(request):
     return Response('Hello World!' % request.matchdict)
 
-def scrape(request):
-	response = urllib2.urlopen("http://www.presidentialrhetoric.com/speeches/01.21.13.html")
-	html_doc = response.read()
-	soup = BeautifulSoup(html_doc)
-	return Response(soup.get_text())
+#Pulling the most common terms from an html doc
+def pull_terms(html, topic):
+	html = html.lower()
+	html = html.translate(None, "!@#$,.()[]{}<>-:\/")
+	text = html.split()
+	count = []
+	if topic == "immigration":
+		count = count_words(text, immigration_list)
+	elif topic == "foreignAffairs":
+		count = count_words(text, foreignAffairs_list)
+	elif topic == "education":
+		count = count_words(text, education_list)
+	'''top_five = []
+	i=0
+	while len(top_five) < 5:
+		word = count[i][0]
+		if word not in black_list and not "=" in word:
+			top_five.append((count[i][0],count[i][1]))
+		i=i+1
+	return top_five'''
+	return count
+
+def count_words(a_list, topic_list):
+    words = {}
+    for i in range(len(a_list)):
+        item = a_list[i]
+        if item in topic_list:
+        	count = a_list.count(item)
+        	words[item] = count
+    return sorted(words.items(), key = lambda item: item[1], reverse=True)
+
+#Scraping for http://www.presidentialrhetoric.com/speeches, will return all the text in an array
+def pres_rhetoric_scrape(date, name, topic):
+	date_term = []
+	full_url = "http://www.presidentialrhetoric.com/campaign2012/" + name + "/" + date + ".html"
+	try: 
+		response = urllib2.urlopen(full_url)
+		html_doc = response.read()
+		terms = pull_terms(html_doc, topic)
+		date_term.append({"date":date, "terms":terms, "full_url":full_url})
+	except:
+		pass
+	response = Response(json.dumps(date_term))
+	return response
+
+def pres_rhetoric_scrape_obama(request):
+	date = request.params.get("date")
+	topic = request.params.get("topic")
+	return pres_rhetoric_scrape(date, "obama", topic)
+
+def pres_rhetoric_scrape_romney(request):
+	date = request.params.get("date")
+	topic = request.params.get("topic")
+	return pres_rhetoric_scrape(date, "romney", topic)
 
 if __name__ == '__main__':
     config = Configurator()
     #adding hello route
     config.add_route('hello', '/hello')
     config.add_view(hello, route_name='hello')
-    #adding scrape route
-    config.add_route('scrape', '/scrape')
-    config.add_view(scrape, route_name='scrape')
+    #adding scrape routes
+    
+    config.add_route('pres_rhetoric_scrape_obama', '/pres_rhetoric_scrape_obama')
+    config.add_view(pres_rhetoric_scrape_obama, route_name='pres_rhetoric_scrape_obama')
+
+    config.add_route('pres_rhetoric_scrape_romney', '/pres_rhetoric_scrape_romney')
+    config.add_view(pres_rhetoric_scrape_romney, route_name='pres_rhetoric_scrape_romney')
+    
     app = config.make_wsgi_app()
     server = make_server('localhost', 8080, app)
     server.serve_forever()
